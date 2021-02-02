eccefb093c9531aca68838dfeb1476c2a10e50f1
aa-ag <aaron.aguerrevere@gmail.com>
2021-02-01 21:06:53 -0600

    using ''.join() to create paragraph, opening test.txt

--- justify.py
+++ justify.py
@@ -3,10 +3,9 @@ from justifytext import justify
 
 
 ###--- GLOBAL VARIABLES ---###
-text = 'hello hello hello hello hello hellohello hello hello'
+text = open('justify_test.txt', 'r')
 width = 20
 
 ###--- FROM DOCUMENTATION ---###
 
-# OPTION 1
-print(justify(text, width))
+print(''.join(justify(text.read(), width)))
